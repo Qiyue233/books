9c64397b3a6a3d8925a0a0328a604d71af703c8e
Qiyue233 <3121001165@qq.com>
2022-09-24 15:15:12 +0800

    初始推送

--- src/test/java/com/example/books/BooksApplicationTests.java
+++ src/test/java/com/example/books/BooksApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BooksApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
