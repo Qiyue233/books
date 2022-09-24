ca4f4cc5a919d261d51a3a63b89cee693ee59d1e
Qiyue233 <3121001165@qq.com>
2022-09-24 15:32:00 +0800

    目录结构更新

--- src/main/java/com/example/books/Application.java
+++ src/main/java/com/example/books/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BooksApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(BooksApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
