a3f7cdeeeda441e135799a323a3aabf6b120a30c
Qiyue233 <3121001165@qq.com>
2022-09-25 14:37:10 +0800

    更正名称

--- src/main/java/com/example/books/controller/BooksController.java
+++ src/main/java/com/example/books/controller/BooksController.java
@@ -1,4 +1,11 @@
 package com.example.books.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
 public class BooksController {
+
 }
--- src/main/java/com/example/books/controller/StaticController.java
+++ src/main/java/com/example/books/controller/StaticController.java
@@ -1,4 +1,12 @@
 package com.example.books.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class StaticController {
+    @GetMapping("index")
+    public String gotoIndex(){
+        return "index";
+    }
 }
