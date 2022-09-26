6719839e584ef24fc0ca7f13d5167ed26a2221b4
Qiyue233 <3121001165@qq.com>
2022-09-26 16:24:02 +0800

    样式

--- src/main/java/com/example/books/controller/BooksController.java
+++ src/main/java/com/example/books/controller/BooksController.java
@@ -3,7 +3,6 @@ package com.example.books.controller;
 
 import com.example.books.bean.Msg;
 import com.example.books.service.BooksService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
--- src/main/java/com/example/books/controller/StaticController.java
+++ src/main/java/com/example/books/controller/StaticController.java
@@ -9,4 +9,9 @@ public class StaticController {
     public String gotoIndex(){
         return "html/index";
     }
+
+    @GetMapping("/main")
+    public String gotoDashboard(){
+        return "html/dashboard";
+    }
 }
